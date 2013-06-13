a22445dafa6c590c53708fbff7bc06b98f4a9ec0
Peter Willert <peter.willert@devbliss.com>
2013-06-13 14:39:22 +0200

    updated indentation

--- dinding-server.js
+++ dinding-server.js
@@ -9,7 +9,6 @@ var twitter = require('ntwitter');
 var mtwitter = require('mtwitter');
 var io      = require('socket.io');
 var http    = require('http');
-var openw   = require('open');
 var config  = require(__dirname + '/config.json');
 var utils   = require(__dirname + '/settings/utils.js');
 var rights  = require(__dirname + '/settings/rights.js')(config);
@@ -22,20 +21,22 @@ var server  = http.createServer(app);
 
 // Express settings
 app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.set('view options', {
-    layout: 'layout'
-  });
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'hbs');
+    'use strict';
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.set('view options', {
+        layout: 'layout'
+    });
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'hbs');
 });
 
 // Routes
 app.get('/', function(req, res) {
-  res.render('index');
+    'use strict';
+    res.render('index');
 });
 
 // ### SOCKET.IO ####
@@ -44,50 +45,52 @@ var socket = require('socket.io').listen(server);
 // Socket settings
 socket.set('log level', 1);
 socket.on('connection', function(sock) {
-  sock.on('beginTransmisson', function() {
-    console.log('Ok! Got your request. Will send some data soon.');
+    'use strict';
+    sock.on('beginTransmisson', function() {
+        console.log('Ok! Got your request. Will send some data soon.');
 
-    // First, send hashtag data
-    sock.emit('hashtags', config.app.hashtags);
+        // First, send hashtag data
+        sock.emit('hashtags', config.app.hashtags);
 
-    app.twSearch(config.app.hashtags, sock);
-  });
+        app.twSearch(config.app.hashtags, sock);
+    });
 });
 
 // Functions used by twit
 app.twSearch = function(keyword, sock) {
-  console.log('## Search started ##', keyword);
+    'use strict';
+    console.log('## Search started ##', keyword);
 
-  for (var i = 0; i < config.app.hashtags.length; i++) {
+    for (var i = 0; i < config.app.hashtags.length; i++) {
+        mtwit.get('search/tweets', {q: config.app.hashtags[i]}, function(err, data) {
 
-      mtwit.get('search/tweets', {q: config.app.hashtags[i]}, function(err, data) {
+            if (!err && data && data.statuses) {
+                console.log('found ', data);
+                data.statuses = data.statuses.reverse();
+                for (var tweet in data.statuses) {
+                    data.statuses[tweet].CHECK = rights.checkUser({
+                        username  : data.statuses[tweet].from_user,
+                        userid    : data.statuses[tweet].from_user_id,
+                        tweetisok : false
+                    }, 'search');
 
-        if (!err && data && data.statuses) {
-          data.statuses = data.statuses.reverse();
-          for (var tweet in data.statuses) {
-            data.statuses[tweet].CHECK = rights.checkUser({
-              username  : data.statuses[tweet].from_user,
-              userid    : data.statuses[tweet].from_user_id,
-              tweetisok : false
-            }, 'search');
+                    if (data.statuses[tweet].CHECK.tweetisok) {
+                        data.statuses[tweet].text = utils.parseHasTags(data.statuses[tweet].text);
+                        data.statuses[tweet].text = utils.parseLinks(data.statuses[tweet].text);
+                        sock.emit('tweetSearch', data.statuses[tweet]);
+                    } else {
+                        console.log('!= ' + data.statuses[tweet].from_user + ' =! blocked!!');
+                    }
+                }
 
-            if (data.statuses[tweet].CHECK.tweetisok) {
-              data.statuses[tweet].text = utils.parseHasTags(data.statuses[tweet].text);
-              data.statuses[tweet].text = utils.parseLinks(data.statuses[tweet].text);
-              sock.emit('tweetSearch', data.statuses[tweet]);
             } else {
-              console.log('!= ' + data.statuses[tweet].from_user + ' =! blocked!!');
+                console.log('err', err);
             }
-          }
+        });
 
-        } else {
-          console.log("err", err);
-        }
-    });
+    }
 
-  }
-
-  app.twStream(sock);
+    app.twStream(sock);
 };
 
 app.twStream = function(sock) {
